e2468e6440782a820de51de0f3fca16627f10345
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-12-23 11:52:23 +0530

    in built module (os)

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+//import the module
+const os=require('os');
+
+
 // open the project file in working directory and open git bash
 // node app.js --> print Welcome to Note JS
-console.log("Welcome to Note JS")
+//console.log("Welcome to Note JS")
 
 const name ="IJSE"
-console.log(name)
+//console.log(name)
 
 /*---------------global object-------------------------------------*/
 // console.log(global)
@@ -16,10 +20,20 @@ global.setInterval(()=>{
 },2000);
 
 /*------------------environment variables-------------------------*/
-console.log(__dirname) // E:\2nd SEMESTER\RAD\nodejsProject
-console.log(__filename) //E:\2nd SEMESTER\RAD\nodejsProject\app.js
+//console.log(__dirname) // E:\2nd SEMESTER\RAD\nodejsProject
+//console.log(__filename) //E:\2nd SEMESTER\RAD\nodejsProject\app.js
 
-/*------------add the noodemon------------------------*/
-console.log("welcome nodemon")
+/*------------After add the noodemon------------------------*/
+//console.log("welcome nodemon")
+
+/*in built module*/
+console.log(os.platform())//win32
+console.log(os.version())//Windows 10 Home Single Language
+console.log(os.tmpdir())//C:\Users\RASHMI~1\AppData\Local\Temp
+console.log(os.release())//10.0.22621
+console.log(os.homedir())//C:\Users\Rashmi Sharmila
+
+
+
 
 
