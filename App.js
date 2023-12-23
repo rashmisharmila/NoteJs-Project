6b2005eb6087aa2cd88a4bf3db94ab6401a7dc0a
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-12-23 11:35:12 +0530

    add the noodemon

--- App.js
+++ App.js
@@ -1,3 +1,25 @@
-// open the project file in working directroy and open gitbash
+// open the project file in working directory and open git bash
 // node app.js --> print Welcome to Note JS
 console.log("Welcome to Note JS")
+
+const name ="IJSE"
+console.log(name)
+
+/*---------------global object-------------------------------------*/
+// console.log(global)
+global.setTimeout(()=>{
+   // console.log("Timeout...!")
+},5000);
+
+global.setInterval(()=>{
+    //console.log("Interval Called..!")
+},2000);
+
+/*------------------environment variables-------------------------*/
+console.log(__dirname) // E:\2nd SEMESTER\RAD\nodejsProject
+console.log(__filename) //E:\2nd SEMESTER\RAD\nodejsProject\app.js
+
+/*------------add the noodemon------------------------*/
+console.log("welcome nodemon")
+
+
